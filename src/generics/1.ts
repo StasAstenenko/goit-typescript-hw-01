da9f3808688701b10e36c1f3125b8729e8dcad4c
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2024-09-09 13:38:11 +0300

    h1-replace

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -3,9 +3,7 @@ import axios from "axios";
 async function fetchData<T>(url: string): Promise<T | undefined> {
   try {
     const response = await axios.get<T>(url);
-    if (typeof response === "object" && response !== null) {
-      return response.data;
-    }
+    return response.data;
   } catch (error: any) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
